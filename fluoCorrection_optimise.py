9c8f375a346a938c5391bcce3a5522500837dcca
msujas <kenneth.marshall@esrf.fr>
2025-07-31 15:37:12 +0200

    update script

--- fluoCorrection_optimise.py
+++ fluoCorrection_optimise.py
@@ -1,9 +1,10 @@
 import numpy as np
-from fluoCorrectionPilatus import fluoSub, optimise_fluo
+from fluoCorrectionPilatus import optimise_fluo, optimiseFluoBins
 
 file = r''
 poni = r''
 k0 = 5e5
-index = 4900
+index = 4800
 iters = 10
-optimise_fluo(file,poni,k0, index, iters)
+#optimise_fluo(file,poni,k0, index, iters)
+optimiseFluoBins(file, poni, k0, 5000, 4800)
