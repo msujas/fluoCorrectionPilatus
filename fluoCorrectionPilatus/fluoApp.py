4b80c4b11f57194645a831225858f4d9d8f4adaf
msujas <kenneth.marshall@esrf.fr>
2025-09-18 14:43:47 +0200

    command line app

---
+++ fluoCorrectionPilatus/fluoApp.py
@@ -0,0 +1,41 @@
+from fluoCorrectionPilatus import *
+import argparse
+from glob import glob
+
+def parseArgs():
+    filedct = {'filename':'image file to perform fluorescence correction on',
+               'directory': 'directory to perform fluorescence correction loop in'}
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str, help = 'filename or directory to run in (default current directory)', nargs='?', default='.')
+    parser.add_argument('-p','--poni', type = str, help = 'poni file for measurement')
+    parser.add_argument('-k','--k0', default=5e5, help= 'starting scaling factor for fluorescence to use')
+    parser.add_argument('-i','--index',default=4800,type = int, help = 'xrd bin to try to flatten in optimisation (default 4800)')
+    args = parser.parse_args()
+    filename = args.file
+    poni = args.poni
+    k0 = args.k0
+    index = args.index
+    return filename, poni, k0, index
+
+
+def runOptimise(file,poni, k0,index):
+    #file,poni, k0,index = parseArgs() #average image file
+    print(file)
+    optimiseFluoBins(file, poni, k0, 5000, index)
+
+def runOptimiseLoop(direc, poniFile, k0, index):
+    #direc, poniFile, k0, index = parseArgs()
+    for root, dirs,files in os.walk(direc):
+        if not 'average' in root or 'xye' in root:
+            continue
+        cbfs = glob(f'{root}/*.cbf')
+        for cbf in cbfs:
+            print(cbf)
+            optimiseFluoBins(cbf, poniFile, k0, 5000, index)
+
+def run():
+    file, poniFile, k0, index = parseArgs()
+    if os.path.isdir(file):
+        runOptimiseLoop(file, poniFile, k0, index)
+    else:
+        runOptimise(file, poniFile, k0, index)
